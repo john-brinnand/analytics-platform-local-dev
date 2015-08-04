151f0f0b01f1992bbfd6ba29a719e517773ab715
Edward Monical-Vuylsteke <edward@spongecell.com>
2015-08-04 14:30:32 -0700

    Adding topics to support retargeting 2.0

--- scripts/kafka_setup.sh
+++ scripts/kafka_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOPICS="audience delta development_items items lists processors publish_request raw_audience_adobe raw_audience_bluekai"
+TOPICS="audience delta development_items items lists processors publish_request raw_audience_adobe raw_audience_bluekai catalog_loader catalog_items"
 
 for topic_to_create in $TOPICS; do
   kafka-topics.sh --zookeeper $(docker-machine ip dev):2181 --create --topic $topic_to_create --replication-factor 1 --partitions 1
